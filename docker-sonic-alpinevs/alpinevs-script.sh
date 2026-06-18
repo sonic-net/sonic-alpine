e1cc3675bcf9e04d12ac50f192128e931db8ae54
Sreemoolanathan Iyer <sbiyer@google.com>
2026-06-18 06:09:33 +0000

    docker-sonic-alpinevs base code
    Includes the makefile and docker-sonic-alpinevs directory
    
    Signed-off-by: Sreemoolanathan Iyer <sbiyer@google.com>

---
+++ docker-sonic-alpinevs/alpinevs-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+# This script now calls pkt-handler at the end and blocks. Because of this,
+# alpinevs-healthcheck.sh expects the script to be always running.
+# If pkt-handler is made to run in the background and this script terminates
+# modify the health script, perhaps handling this as a special case.
+
+/usr/bin/alpinevs-init.sh
+/usr/bin/alpinevs-config.sh
+/usr/bin/pkt-handler
