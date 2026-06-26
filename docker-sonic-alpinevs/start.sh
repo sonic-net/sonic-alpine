ff984b09132624eb1087472f6917a13d03bf8150
Sreemoolanathan Iyer <sbiyer@google.com>
2026-06-26 13:18:54 +0000

    copp_cfg related changes
    Signed-off-by: Sreemoolanathan Iyer <sbiyer@google.com>

--- docker-sonic-alpinevs/start.sh
+++ docker-sonic-alpinevs/start.sh
@@ -24,7 +24,6 @@ if [[ -f /usr/share/sonic/virtual_chassis/default_config.json ]]; then
     mv /tmp/init_cfg.json /etc/sonic/init_cfg.json
 fi
 
-# Sree - new processing of config_db.json
 if [ -f /etc/sonic/config_db.json ]; then
     # Merge basic system init properties into config_db.json
     sonic-cfggen -j /etc/sonic/init_cfg.json -j /etc/sonic/config_db.json --print-data > /tmp/config_db.json
@@ -35,7 +34,7 @@ else
     exit 1
 fi
 
-sonic-cfggen -t /usr/share/sonic/templates/copp_cfg.j2 > /etc/sonic/copp_cfg.json
+sonic-cfggen -t /usr/share/sonic/templates/copp_cfg_alpinevs.j2 > /etc/sonic/copp_cfg.json
 
 mkdir -p /etc/swss/config.d/
 
