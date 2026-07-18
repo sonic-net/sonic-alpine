eeb1f0d0943c37ba6b64824d2ebb1cdfe82698a4
Your Name <you@example.com>
2026-07-18 08:30:46 +0000

    Rebase
    Signed-off-by: Sreemoolanathan Iyer <sbiyer@google.com>

--- docker-sonic-alpinevs.mk
+++ docker-sonic-alpinevs.mk
@@ -18,6 +18,7 @@ $(DOCKER_SONIC_ALPINEVS)_DEPENDS += $(SYNCD_VS) \
                               $(SONIC_TELEMETRY) \
                               $(SONIC_MGMT_FRAMEWORK) \
                               $(SONIC_MGMT_COMMON)
+                              $(SONIC_P4RT)
 
 $(DOCKER_SONIC_ALPINEVS)_PYTHON_WHEELS += $(SONIC_PY_COMMON_PY3) \
                                     $(SONIC_PLATFORM_COMMON_PY3) \
