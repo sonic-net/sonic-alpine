7a620dc611bc81eb620c164ff76b75867017498c
Sree Iyer <sbiyer@google.com>
2026-09-09 20:59:40 +0530

    [AVS-lite] Moved config_db as a config option (#48)
    Signed-off-by: Sree Iyer <sbiyer@google.com>

--- docker-sonic-alpinevs/start.sh
+++ docker-sonic-alpinevs/start.sh
@@ -24,12 +24,13 @@ if [[ -f /usr/share/sonic/virtual_chassis/default_config.json ]]; then
     mv /tmp/init_cfg.json /etc/sonic/init_cfg.json
 fi
 
-if [ -f /etc/sonic/config_db.json ]; then
+if [ -f /mnt/sonic/config_db.json ]; then
+    cp /mnt/sonic/config_db.json /etc/sonic/config_db.json
     # Merge basic system init properties into config_db.json
     sonic-cfggen -j /etc/sonic/init_cfg.json -j /etc/sonic/config_db.json --print-data > /tmp/config_db.json
     mv /tmp/config_db.json /etc/sonic/config_db.json
     echo "Using existing config_db.json"
-else
+elif [ ! -f /etc/sonic/config_db.json ]; then
     echo "ERROR: /etc/sonic/config_db.json is missing! Cannot initialize system."
     exit 1
 fi
