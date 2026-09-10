74c287b4b467e086584b8dec8e0cdb0f06ef081f
Sree Iyer <sbiyer@google.com>
2026-09-10 14:08:44 +0000

    Enable auto-complete in root login
    1. Made auto complete to work in the root login also
    2. Removed copy of hwsku.json
    3. Upgraded deploy/vm/Dockerfile to a more recent Ubuntu
    4. Added checks and retry in the docker save script
    5. pkt-handler was being rebuilt as a safety option and this was adding random
    build logs on the console. Removed this
    6. A script to download the official alpine build from github
    
    Signed-off-by: Sree Iyer <sbiyer@google.com>

--- docker-sonic-alpinevs.mk
+++ docker-sonic-alpinevs.mk
@@ -46,14 +46,12 @@ SONIC_DOCKER_IMAGES += $(DOCKER_SONIC_ALPINEVS)
 SONIC_TRIXIE_DOCKERS += $(DOCKER_SONIC_ALPINEVS)
 
 ALPINEVS_DOCKER_STAGING_DIR := $(PLATFORM_PATH)/docker-sonic-alpinevs/bin
-ALPINEVS_PKT_HANDLER_SRC_DIR := $(PLATFORM_PATH)/src/services/pkt-handler
 ALPINEVS_CONFIG_SRC := $(PLATFORM_PATH)/src/services/config/alpinevs-config.sh
 ALPINEVS_INIT_SRC := $(PLATFORM_PATH)/docker-sonic-alpinevs/alpinevs-init.sh
 ALPINEVS_ADMIN_USERNAME_FILE := $(ALPINEVS_DOCKER_STAGING_DIR)/sonic-admin.username
 ALPINEVS_ADMIN_PASSWORD_HASH := $(ALPINEVS_DOCKER_STAGING_DIR)/sonic-admin.password.hash
 
 ALPINEVS_DOCKER_STAGE_FILES := \
-	$(ALPINEVS_DOCKER_STAGING_DIR)/pkt-handler \
 	$(ALPINEVS_DOCKER_STAGING_DIR)/alpinevs-config.sh \
 	$(ALPINEVS_DOCKER_STAGING_DIR)/alpinevs-init.sh \
 	$(ALPINEVS_ADMIN_USERNAME_FILE) \
@@ -76,13 +74,6 @@ $(ALPINEVS_ADMIN_PASSWORD_HASH): | $(ALPINEVS_DOCKER_STAGING_DIR)
 	else \
 		printf '%s\n' '!' > "$@"; \
 	fi
-
-# Always rebuild it before staging the Docker build-context copy
-.PHONY: $(ALPINEVS_DOCKER_STAGING_DIR)/pkt-handler
-$(ALPINEVS_DOCKER_STAGING_DIR)/pkt-handler: | $(ALPINEVS_DOCKER_STAGING_DIR)
-	$(MAKE) -C $(ALPINEVS_PKT_HANDLER_SRC_DIR) clean
-	$(MAKE) -C $(ALPINEVS_PKT_HANDLER_SRC_DIR) all
-	cp $(ALPINEVS_PKT_HANDLER_SRC_DIR)/pkt-handler $@
 
 $(ALPINEVS_DOCKER_STAGING_DIR)/alpinevs-config.sh: \
 		$(ALPINEVS_CONFIG_SRC) | $(ALPINEVS_DOCKER_STAGING_DIR)
